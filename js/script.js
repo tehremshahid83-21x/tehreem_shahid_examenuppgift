2966712bdcaa5dbf89fed4ca671ee33497f4be8a
tehremshahid83-21x <tehremshahid83@gmail.com>
2026-06-09 22:36:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-constsound=document.getElementById("keysound");
+const sound= document.getElementById("fil");
+console.log(sound);
+constsound= document.getElementById("fil");
     document.querySelectorAll(".key"). forEach(key=>{
         key.addEventListener("click", () => {
             sound.currentTime=0;
             sound.play();
             key.classList.add("active");
 
-            setrTimeout(() =>{
+            setrTimeout(() => {
                 key.classList.remove("active");
             } ,150);
          });
-        });
+ });
